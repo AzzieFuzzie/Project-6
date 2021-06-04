39d922ca9d53b9a2a1520f2eb8974dc80d27e1dc
AzzieFuzzie <azziefuzzie01@gmail.com>
2021-06-04 10:47:48 +0200

    update

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { data } = require('../data.json');
+
+const app = express();
+app.set('view engine', 'pug');
+
+app.use('/static', express.static('public'));
+
+app.get('/about', (req, res, next) => {
+  res.render('about');
+  next();
+});
+app.get('/', (req, res) => {
+  res.render('data.json', {});
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.listen(3000, () => {
+  console.log('listening');
+});

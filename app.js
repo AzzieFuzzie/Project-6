28b681f4c826f5d1ae9d3681ea7c0c7a162e8300
Azzie Fuzzie <azziefuzzie01@gmail.com>
2021-06-30 17:04:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 const router = express.Router();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Requires data from data.json file
 const { projects } = require('./data.json');
